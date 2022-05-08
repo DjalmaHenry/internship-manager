dceb9e0e049d214cb0e3d63288bdc958cf666d6e
Joao Vitor <outlast4321@gmail.com>
2022-05-08 20:27:38 -0300

    Update Contrato.js

--- backend/model/Contrato.js
+++ backend/model/Contrato.js
@@ -1,9 +1,8 @@
 const Aluno = require("./Aluno")
-
 module.exports = class Contrato {
     constructor(id, Aluno, Empresa, Vaga, status) {
         this.id = id
-        this.Aluno = Aluno
+        Aluno = Aluno
         this.Empresa = Empresa
         this.Vaga = Vaga
         this.status = status
