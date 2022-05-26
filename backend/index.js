7fc07f8cdc23b8a1d4d8b8bccca8804fee4c7702
Djalma Henrique <djalma@geppetto.com.br>
2022-05-25 22:42:46 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const app = require('./server')
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
