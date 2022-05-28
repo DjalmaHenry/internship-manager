6d1665473c4e1281f99db872e943a8b33f7ccc34
Djalma Henrique <djalma@geppetto.com.br>
2022-05-28 15:31:34 -0300

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -6,10 +6,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, './build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  res.sendFile(path.join(__dirname, './build'));
 });
 
 const PORT = process.env.PORT || 3000;
