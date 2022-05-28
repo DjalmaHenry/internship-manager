2e624441e0fce84d5fe6f1bea9e4d3aa8a082e19
Djalma Henrique <djalma@geppetto.com.br>
2022-05-28 15:36:49 -0300

    feat: remove

--- frontend/server.js
+++ frontend/server.js
@@ -1,19 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+
+const { resolve } = require("path");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, './build')));
+app.use("/", express.static(resolve(__dirname, "./build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build'));
-});
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    return console.log(err);
+  }
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log("Tudo funcionando corretamente");
 });
