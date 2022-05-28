292adc289fabbc9b4e0df43a6dd04a90c9168957
Djalma Henrique <djalma@geppetto.com.br>
2022-05-28 15:22:20 -0300

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -4,7 +4,7 @@ const { resolve } = require("path");
 
 const app = express();
 
-app.use("/", express.static(resolve(__dirname, "./build")));
+app.use("*", express.static(resolve(__dirname, "./build")));
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
