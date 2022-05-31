626e0561cf39b72f4162f3ecbca4e3fd79b00799
Djalma Henrique <djalma@geppetto.com.br>
2022-05-30 23:23:11 -0300

    fix

--- frontend/server.js
+++ frontend/server.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "./build/static")));
+app.use(express.static(path.join(__dirname, "./build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
 app.listen(process.env.PORT || 3000, (err) => {
