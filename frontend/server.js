b8807d83361dc71ed259b663fb15576c7307f819
Djalma Henrique <djalma@geppetto.com.br>
2022-05-30 23:34:06 -0300

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,20 +1,19 @@
-const express = require("express");
-
-const { resolve } = require("path");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(express.static(resolve(__dirname, "./build")));
+app.use(express.static(path.join(__dirname, './build')));
 
-app.get("*", (req, res) => {
-  res.sendFile(resolve(__dirname, "./build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
-app.listen(process.env.PORT || 3000, (err) => {
-  if (err) {
-    return console.log(err);
-  }
+const PORT = process.env.PORT || 3000;
 
-  console.log("Tudo funcionando corretamente");
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
